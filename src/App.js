aa8f143db55be02391af07162e8a8442de881855
Chetan kaushik <chetankaushik29@gmail.com>
2024-03-29 20:12:39 +0530

    Signup and sing In Basic created

--- src/App.js
+++ src/App.js
@@ -2,24 +2,53 @@
 import { useState } from 'react';
 import './App.css';
 import app from './firebase';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
 function App() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  const auth= getAuth();
+  const auth = getAuth();
 
- async function handleSubmit() {
-    const result=await createUserWithEmailAndPassword(auth,email,password);
-    console.log(result)
+  async function handleSubmitSignup() {
+    try {
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(result)
+      alert("Signup Successfull")
+    } catch (error) {
+      console.log(error)
+      alert(error.message)
+    }
+
+  }
+
+  async function handleSubmitSignIn() {
+    try {
+      const signIn = await signInWithEmailAndPassword(auth, email, password);
+      console.log(signIn);
+      alert("User Signed In")
+    } catch (error) {
+      console.log(error)
+      alert(error.message)
+    }
   }
   return (
-    <div className="App">
-      <input type="email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit" onClick={handleSubmit}>Login</button>
+    <div className="App" style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space_evenly", height: "100vh" }}>
+      <div >
+
+
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+
+        <button type="submit" onClick={handleSubmitSignup} >SignUp</button>
+      </div>
+      <br />
+      <div>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit" onClick={handleSubmitSignIn}>SignIn</button>
+      </div>
     </div>
   );
 }
